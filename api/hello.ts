6e7712bd3b5ff3eff7eb0147fa9eadac57c8d26c
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2022-02-17 11:03:02 +0700

    redeploy vercel 3

--- api/hello.ts
+++ api/hello.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 // It is important that the full path is specified here
-app.get('/', function (_, res) {
+app.get('/api/hello', function (_, res) {
   res.status(200).json({ message: 'This is hello message' })
 })
 
